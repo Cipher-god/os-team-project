3e1d4b6518df25c2985ef3006cc0c29a795a8ac0
Cipher-god <90594902+Cipher-god@users.noreply.github.com>
2023-02-06 21:34:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const typing = () => {
     if (i < text.length) {
       document.getElementById("heading").innerHTML += text.charAt(i);
       i++;
-      setTimeout(typing, 100);
+      setTimeout(typing,70);
     }
 }
 let i2 = 0;
-const text2 = '\nCalculate Process Scheduling Algorithms';
+const text2 = 'Calculate Process Scheduling Algorithms';
 const typing2 = () => {
     if (i2 < text2.length) {
       document.getElementById("algo").innerHTML += text2.charAt(i2);
       i2++;
-      setTimeout(typing2, 100);
+      setTimeout(typing2, 70);
     }
 }
-typing();
-typing2();
+setTimeout(typing,0);
+setTimeout(typing2,5000);
