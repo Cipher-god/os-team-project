52394997602573e59c54822745cd7bad7b712ee9
Cipher-god <90594902+Cipher-god@users.noreply.github.com>
2023-02-12 10:04:08 +0000

    bundle.js

--- bundle.js
+++ bundle.js
@@ -266,15 +266,12 @@ exports.clearImmediate = typeof clearImmediate === "function" ? clearImmediate :
 },{"process/browser.js":1,"timers":2}],3:[function(require,module,exports){
 "use strict";
 
-var MongoClient = require('mongoose');
-var uri = "mongodb+srv://osproject_admin:OsProject908@cluster.mongodb.net/scheduling_algos?retryWrites=true&w=majority";
-var client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-client.connect(function (err) {
+const MongoClient = require('mongoose');
+const uri = "mongodb+srv://osproject_admin:OsProject908@cluster.mongodb.net/scheduling_algos?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
   if (err) throw err;
-  var db = client.db("scheduling_algos");
+  const db = client.db("scheduling_algos");
   console.log("Connected to MongoDB!");
   client.close();
 });
