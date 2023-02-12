664faba26e923ac33c7baedcab0dc176d7738eba
Cipher-god <90594902+Cipher-god@users.noreply.github.com>
2023-02-12 07:06:39 +0000

    app.js

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-// const { MongoClient } = require("mongodb");
-// const uri = "mongodb+srv://osproject_admin:OsProject908@cluster.mongodb.net/scheduling_algos?retryWrites=true&w=majority";
-
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//   if (err) throw err;
-//   const db = client.db("scheduling_algos");
-//   console.log("Connected to MongoDB!");
-//   client.close();
-// });
-const mongodb = require("mongodb");
-console.log(mongodb.version);
+import { MongoClient } from "mongodb";
+const uri = "mongodb+srv://osproject_admin:OsProject908@cluster.mongodb.net/scheduling_algos?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  if (err) throw err;
+  const db = client.db("scheduling_algos");
+  console.log("Connected to MongoDB!");
+  client.close();
+});
