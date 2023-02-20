7094f3c81892a531ac84009902ce4d689bbdc10b
Cipher-god <90594902+Cipher-god@users.noreply.github.com>
2023-02-20 13:50:16 +0000

    main

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://osproject_admin:OsProject908@cluster0.kwcneuy.mongodb.net/shedulingprocesses?retryWrites=true&w=majority";
-
+const uri = "mongodb+srv://admin:osOS1234@cluster0.kwcneuy.mongodb.net/shedulingprocesses?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
+  console.log('Connected successfully to MongoDB');
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
   client.close();
