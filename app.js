1fbc3168ed8d975c36ebc9448ba3ab00a3fad702
Cipher-god <90594902+Cipher-god@users.noreply.github.com>
2023-04-03 15:31:02 +0000

    dbupdate4

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ async function getData()
   let collection = db.collection('pps');
   let response = await collection.find({}).toArray()
   console.log(response);
+  document.getElementById("dataps");
   client.close();
 }
 getData();
